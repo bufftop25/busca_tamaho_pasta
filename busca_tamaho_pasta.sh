59f849057eef08cd9cc7aeee416d1b583180f61e
bufftop25 <isaias2011amaral@gmail.com>
2024-07-11 00:01:44 -0300

    first commit

--- busca_tamaho_pasta.sh
+++ busca_tamaho_pasta.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-wget -O /home/deploy/atualiza_public.sh https://raw.githubusercontent.com/bufftop25/busca_tamaho_pasta/main/busca_tamaho_pasta.sh
+wget -O /home/deploy/atualiza_public.sh https://raw.githubusercontent.com/FilipeCamillo/busca_tamaho_pasta/main/busca_tamaho_pasta.sh
 # Carregar variáveis de ambiente do arquivo .env
+source "/home/deploy/workzap/backend/.env"
+
 # Caminho para a pasta public
 PUBLIC_FOLDER="/home/deploy/workzap/backend/public"
 
