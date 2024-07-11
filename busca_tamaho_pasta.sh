4f021887cf8d9273c6b527755afad5a1dca1ff95
bufftop25 <isaias2011amaral@gmail.com>
2024-07-11 00:53:57 -0300

    NOVA VERSÃO

--- busca_tamaho_pasta.sh
+++ busca_tamaho_pasta.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+
+# Baixar o script de atualização
 wget -O /home/deploy/atualiza_public.sh https://raw.githubusercontent.com/bufftop25/busca_tamaho_pasta/main/busca_tamaho_pasta.sh
-# Carregar variáveis de ambiente do arquivo .env
-source "/home/deploy/${empresa}/backend/.env"
 
-# Caminho para a pasta public
-PUBLIC_FOLDER="/home/deploy/${empresa}/backend/public"
+# Encontrar todas as pastas que contêm o diretório backend
+BASE_DIRS=$(find /home/deploy/ -type d -name backend | sed 's|/backend||')
 
-# Testar a conexão com o banco de dados
-if PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1" >/dev/null; then
-    echo "Conexão com o banco de dados bem-sucedida."
-else
-    echo "Erro: Não foi possível conectar ao banco de dados."
+# Verificar se algum diretório backend foi encontrado
+if [ -z "$BASE_DIRS" ]; then
+    echo "Erro: Não foi possível encontrar nenhum diretório backend."
     exit 1
 fi
 
@@ -26,34 +24,58 @@ get_current_date() {
     date +"%Y-%m-%d %H:%M:%S"
 }
 
-# Loop para processar cada pasta na pasta public
-for folder in "$PUBLIC_FOLDER"/*; do
-    if [ -d "$folder" ]; then
-        folder_name=$(basename "$folder")
-        # Extrair o ID da company do nome da pasta
-        company_id=$(extract_numbers "$folder_name")
-        
-        # Verificar se a company existe antes de tentar atualizar seus dados
-        if PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -t -c "SELECT EXISTS (SELECT 1 FROM public.\"Companies\" WHERE id = '$company_id');" | grep -q 't'; then
-            num_files=$(find "$folder" -type f | wc -l)
-            folder_size=$(du -sh "$folder" | awk '{print $1}')
-            update_date=$(get_current_date)
+# Processar cada diretório que contém o backend
+for BASE_DIR in $BASE_DIRS; do
 
-             # Comando SQL para realizar a atualização
-            sql_command="UPDATE public.\"Companies\" 
-                         SET \"folderSize\" = '$folder_size', 
-                             \"numberFileFolder\" = '$num_files', 
-                             \"updatedAtFolder\" = '$update_date' 
-                         WHERE id = '$company_id';"
-
-            # Executar o comando SQL utilizando psql
-            PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$sql_command"
-            
-            echo "Dados da empresa ID $company_id atualizados com sucesso."
-        else
-            echo "Erro: A empresa com o ID $company_id não foi encontrada."
-        fi
+    # Carregar variáveis de ambiente do arquivo .env
+    if [ -f "$BASE_DIR/backend/.env" ]; then
+        source "$BASE_DIR/backend/.env"
+    else
+        echo "Erro: Arquivo .env não encontrado em $BASE_DIR/backend."
+        continue
     fi
+
+    # Caminho para a pasta public
+    PUBLIC_FOLDER="$BASE_DIR/backend/public"
+
+    # Testar a conexão com o banco de dados
+    if PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "SELECT 1" >/dev/null; then
+        echo "Conexão com o banco de dados bem-sucedida para $BASE_DIR."
+    else
+        echo "Erro: Não foi possível conectar ao banco de dados para $BASE_DIR."
+        continue
+    fi
+
+    # Loop para processar cada pasta na pasta public
+    for folder in "$PUBLIC_FOLDER"/*; do
+        if [ -d "$folder" ]; then
+            folder_name=$(basename "$folder")
+            # Extrair o ID da company do nome da pasta
+            company_id=$(extract_numbers "$folder_name")
+            
+            # Verificar se a company existe antes de tentar atualizar seus dados
+            if PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -t -c "SELECT EXISTS (SELECT 1 FROM public.\"Companies\" WHERE id = '$company_id');" | grep -q 't'; then
+                num_files=$(find "$folder" -type f | wc -l)
+                folder_size=$(du -sh "$folder" | awk '{print $1}')
+                update_date=$(get_current_date)
+
+                # Comando SQL para realizar a atualização
+                sql_command="UPDATE public.\"Companies\" 
+                             SET \"folderSize\" = '$folder_size', 
+                                 \"numberFileFolder\" = '$num_files', 
+                                 \"updatedAtFolder\" = '$update_date' 
+                             WHERE id = '$company_id';"
+
+                # Executar o comando SQL utilizando psql
+                PGPASSWORD="$DB_PASS" psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$sql_command"
+                
+                echo "Dados da empresa ID $company_id atualizados com sucesso para $BASE_DIR."
+            else
+                echo "Erro: A empresa com o ID $company_id não foi encontrada para $BASE_DIR."
+            fi
+        fi
+    done
+
 done
 
 echo "Rotina concluída"
